2ce63661cb1f723996d457817fd6005c2c6a5c3c
viswa1044 <118976978+viswa1044@users.noreply.github.com>
2022-11-25 09:23:50 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -4,6 +4,6 @@
 echo "This is an Samples Script from GitHub to update to work"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
-echo "This is running in a build step and it shows changes it will update"
+echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
